cd34a1a148fcb9e848926476424292d2f3184a0b
samuel rigo <samuelrigo@gmail.com>
2026-09-21 17:27:19 -0300

    fix: correct tarefa retrieval logic in GET /tarefas/:id endpoint

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ app.get("/tarefas", (req, res) => {
   res.json(tarefas);
 });
 
+app.get("/tarefas/:id", (req, res) => {
+  console.log(req.params);
+
+  const id = Number(req.params.id);
+  const tarefa = tarefas.find((t) => t.id === id);
+
+  if (!tarefa) {
+    res.status(404).json("Não foi possível encontrar a tarefa");
+  }
+  res.status(200).json(tarefa);
+});
+
 app.post("/tarefas", (req, res) => {
   const novaTarefa = {
     id: tarefas.length + 1,
