3e71a6e2242fb3601f57582fe00a54c985b64294
samuel rigo <samuelrigo@gmail.com>
2026-09-23 17:54:48 -0300

    feat: add API key validation middleware for enhanced security

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 
+CHAVES_VALIDADAS = ["abc123", "def456"];
+
+const verificarApiKey = (req, res, next) => {
+  const chave = req.headers["x-api-key"];
+
+  if (!chave) {
+    return res.status(401).json("Chave de API obrigatório");
+  }
+
+  if (!CHAVES_VALIDADAS.includes(chave)) {
+    return res.status(403).json("Chave de API inválida");
+  }
+  next();
+};
+
 const tarefaRouter = require("./routes/tarefas");
 const comentariosRouter = require("./routes/comentarios");
 
 app.use(express.json());
+app.use(verificarApiKey);
 app.use("/tarefas", tarefaRouter);
 app.use("/tarefas/:tarefaId/comentarios", comentariosRouter);
 
